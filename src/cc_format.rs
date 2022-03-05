85d35030a7171100d1bff2299775dcf37ca44d03
olivier de Meringo <demeringo@gmail.com>
2022-03-05 13:17:13 +0100

    feat: test reading cc data

--- src/cc_format.rs
+++ src/cc_format.rs
@@ -1,9 +1,10 @@
-use serde_derive::Deserialize;
-use serde_derive::Serialize;
+use serde::Deserialize;
+use serde::Serialize;
+
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Root {
+pub struct CarbonCrushResult {
     pub value: String,
     pub appid: String,
     pub cijob: String,
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use serde::Deserialize;
 use std::fs::File;
 use std::path::Path;
+mod cc_format;
+use crate::cc_format::CarbonCrushResult;
+
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -12,13 +15,18 @@ struct User {
 }
 
 fn main() {
-    let json_file_path = Path::new("../tests/sample.json");
+    let json_file_path = Path::new("./tests/measure-summary.json");
     let file = File::open(json_file_path).expect("file not found");
-    let users: Vec<User> = serde_json::from_reader(file).expect("error while reading");
+    let ccres: CarbonCrushResult = serde_json::from_reader(file).expect("error while reading");
+
+    println!(
+        "Hello {} aka {} {}",
+        ccres.appid, ccres.cipipelineurl, ccres.value)
+    /*let users: Vec<User> = serde_json::from_reader(file).expect("error while reading");
     for user in users {
         println!(
             "Hello {} aka {} {}",
             user.username, user.first_name, user.last_name
         )
-    }
+    }*/
 }
