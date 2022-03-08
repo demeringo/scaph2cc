ab14400316f764e2ccf253babefe2b692b32efb9
olivier de Meringo <demeringo@gmail.com>
2022-03-08 18:05:59 +0100

    feat: add minimal cli

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # scaph2cc
-a small utility to filter and convert Scaphandre json output to a format suitable for CarbonCrush
+
+A small utility to filter and convert Scaphandre json output to a format suitable for CarbonCrush.
+
+## Usage
+
+```bash
+cargo run -- -i tests/scaphandre-full-report.json -o tests/carbon-crush-data.json --app-id 123 --branch master --ci-pipeline-url "http://ci.com/master/123456" --process-name stress-ng
+```
+
+```bash
+
+scaph2cc --help (or cargo run -- --help)
+
+Filter Scaphandre json report on a specific process name and convert it to carbon crush json format
+
+USAGE:
+    scaph2cc --input-file <INPUT_FILE> --output-file <OUTPUT_FILE> --process-name <PROCESS_NAME> --app-id <APP_ID> --branch <BRANCH> --ci-pipeline-url <CI_PIPELINE_URL>
+
+OPTIONS:
+    -a, --app-id <APP_ID>                      Carbon crush app id
+    -b, --branch <BRANCH>                      Name of the carbon crush file to generate
+    -c, --ci-pipeline-url <CI_PIPELINE_URL>    ci pipeline URL
+    -h, --help                                 Print help information
+    -i, --input-file <INPUT_FILE>              Name of the scaphandre input file
+    -o, --output-file <OUTPUT_FILE>            Name of the carbon crush file to generate
+    -p, --process-name <PROCESS_NAME>          The process name to filter
+    -V, --version                              Print version information
+```
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,47 @@ use crate::cc_format::*;
 
 mod scaph_reader;
 
-fn main() {
-    let app_id = "myapp1";
-    let branch = "main";
-    let pipeline_url = "http://test/url";
+use clap::Parser;
 
-    let scaph_filename = "scaphandre-full-report.json";
-    let cc_filename = "carbon-crush-data.json";
-    let process_name = "stress-ng";
+/// Filter Scaphandre json report on a specific process name and convert it to carbon crush json format
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the scaphandre input file
+    #[clap(short, long)]
+    input_file: String,
+
+    /// Name of the carbon crush file to generate
+    #[clap(short, long)]
+    output_file: String,
+
+    /// The process name to filter
+    #[clap(short, long)]
+    process_name: String,
+
+    /// Carbon crush app id
+    #[clap(short, long)]
+    app_id: String,
+
+    /// Name of the carbon crush file to generate
+    #[clap(short, long)]
+    branch: String,
+
+    /// ci pipeline URL
+    #[clap(short, long)]
+    ci_pipeline_url: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let app_id = args.app_id.as_str();
+    let branch = args.branch.as_str();
+    let pipeline_url = args.ci_pipeline_url.as_str();
+    let scaph_filename = args.input_file.as_str();
+    let cc_filename = args.output_file.as_str();
+    let process_name = args.process_name.as_str();
 
     let average_consumption = scaph_reader::average_consumption(scaph_filename, process_name);
-
     let duration = scaph_reader::process_duration_seconds(scaph_filename, process_name);
 
     let total_energy = duration * average_consumption;
