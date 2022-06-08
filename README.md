d80afa5a09e0e8a27c6a0583e25427d1807d216d
olivier de Meringo <demeringo@gmail.com>
2022-06-08 17:28:40 +0200

    feat: produce junit report

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A small utility to filter and convert ⚡ [Scaphandre](https://github.com/hubblo
 ## Usage
 
 The CLI expects that you provide:
+
 - the scaphandre json analysis file as input
 - the name of the file to be generated
 - the process name that you want to use to filter the input file
@@ -12,35 +13,53 @@ The CLI expects that you provide:
   - application id
   - ci branch measured
   - url of the current ci pipeline
-  - commit id 
+  - commit id
 
-It returns a json summary file that can be ingested by CarbonCrush API.
+It returns a json summary file that can be ingested by CarbonCrush API and a junit test report.
 
 ```bash
-cargo run -- -i tests/scaphandre-full-report.json -o tests/carbon-crush-data.json --app-id myapp-123 --branch master --ci-pipeline-url "http://ci.com/master/123456" --process-name stress-ng --commit-sha d50e3b5ed5c27a848008abd5beb3d9e6c37c3f33
+cargo run -- -i tests/scaphandre-full-report.json -o tests/carbon-crush-data.json --junit-report-file junit-report.xml --app-id myapp-123 --branch master --ci-pipeline-url "http://ci.com/master/123456" --process-name stress-ng --commit-sha d50e3b5ed5c27a848008abd5beb3d9e6c37c3f33
 ```
 
 ```bash
 
-scaph2cc --help (or cargo run -- --help)
-
-
-Filter Scaphandre json report on a specific process name and convert it to carbon crush json format
+scaph2cc 0.2.0
+Olivier de Meringo <demeringo@gmail.com>
+The arguments of scap2cc CLi
 
 USAGE:
-    scaph2cc --input-file <INPUT_FILE> --output-file <OUTPUT_FILE> --process-name <PROCESS_NAME> --app-id <APP_ID> --branch <BRANCH> --ci-pipeline-url <CI_PIPELINE_URL> --commit-sha <COMMIT_SHA>
+    scaph2cc --input-file <INPUT_FILE> --output-file <OUTPUT_FILE> --junit-report-file <JUNIT_REPORT_FILE> --process-name <PROCESS_NAME> --app-id <APP_ID> --branch <BRANCH> --ci-pipeline-url <CI_PIPELINE_URL> --commit-sha <COMMIT_SHA>
 
 OPTIONS:
-    -a, --app-id <APP_ID>                      Carbon crush app id
-    -b, --branch <BRANCH>                      Name of the carbon crush file to generate
-    -c, --commit-sha <COMMIT_SHA>              commit identifier (sha)
-    -h, --help                                 Print help information
-    -i, --input-file <INPUT_FILE>              Name of the scaphandre input file
-    -o, --output-file <OUTPUT_FILE>            Name of the carbon crush file to generate
-    -p, --process-name <PROCESS_NAME>          The process name to filter
-    -u, --ci-pipeline-url <CI_PIPELINE_URL>    ci pipeline URL
-    -V, --version                              Print version information
- olivier@pad  ~/atelier/scaph/scaph2cc 
+    -a, --app-id <APP_ID>
+            Carbon crush app id
+
+    -b, --branch <BRANCH>
+            Name of the current branch
+
+    -c, --commit-sha <COMMIT_SHA>
+            commit identifier (sha)
+
+    -h, --help
+            Print help information
+
+    -i, --input-file <INPUT_FILE>
+            Name of the scaphandre result file to use as input
+
+    -j, --junit-report-file <JUNIT_REPORT_FILE>
+            Name of the junit report to generate
+
+    -o, --output-file <OUTPUT_FILE>
+            Name of the carbon crush file to generate
+
+    -p, --process-name <PROCESS_NAME>
+            The name of the process to filter
+
+    -u, --ci-pipeline-url <CI_PIPELINE_URL>
+            ci pipeline URL
+
+    -V, --version
+            Print version information
 ```
 
 ### Carbon Crush input Format
--- src/carboncrush_exporter.rs
+++ src/carboncrush_exporter.rs
@@ -1,7 +1,10 @@
+use std::fmt;
 use std::path::PathBuf;
 
 use serde::Deserialize;
 use serde::Serialize;
+
+use quick_junit::*;
 
 /// The structure that describe Carbon Crush input
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -16,9 +19,15 @@ pub struct CarbonCrushResult {
     pub ci_pipeline_url: String,
 }
 
+impl fmt::Display for CarbonCrushResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "consumption: {}", self.consumption)
+    }
+}
+
 /// Save a carbon crush results as a file
 pub fn save_carboncrush_file(
-    carbon_crush_result: CarbonCrushResult,
+    carbon_crush_result: &CarbonCrushResult,
     carboncrush_json_file: PathBuf,
 ) {
     println!(
@@ -28,9 +37,15 @@ pub fn save_carboncrush_file(
     // Save the JSON structure into the other file.
     std::fs::write(
         carboncrush_json_file,
-        serde_json::to_string_pretty(&carbon_crush_result).unwrap(),
+        serde_json::to_string_pretty(carbon_crush_result).unwrap(),
     )
     .unwrap();
+}
+
+pub fn save_as_junit_report(carbon_crush_result: &CarbonCrushResult, report_file: PathBuf) {
+    let report = build_junit_report(carbon_crush_result);
+    // Save the junit report into file.
+    std::fs::write(report_file, report.to_string().unwrap()).unwrap();
 }
 
 /// Build a carbon crush data structure with the passed values.
@@ -53,6 +68,22 @@ pub fn build_carboncrush_result(
         commit_sha: commit_sha.to_string(),
         duration: duration.to_string(),
     }
+}
+
+pub fn build_junit_report(carbon_crush_result: &CarbonCrushResult) -> Report {
+    let mut report = Report::new("Power measure");
+    let test_suite_name = carbon_crush_result.app_id.as_str();
+    let mut test_suite = TestSuite::new(test_suite_name);
+    let mut success_case = TestCase::new(
+        carbon_crush_result.commit_sha.as_str(),
+        TestCaseStatus::success(),
+    );
+
+    success_case.set_system_out(format!("{:?}", carbon_crush_result));
+
+    test_suite.add_test_cases([success_case]);
+    report.add_test_suite(test_suite);
+    report
 }
 
 #[cfg(test)]
@@ -83,6 +114,45 @@ mod tests {
             1230.0,
             10.0,
         );
-        save_carboncrush_file(carboncrush_result, carboncrush_json_file)
+        save_carboncrush_file(&carboncrush_result, carboncrush_json_file)
+    }
+    #[test]
+    fn test_build_junit_report() {
+        let carboncrush_result = build_carboncrush_result(
+            123.0,
+            "myapp1",
+            "main",
+            "d50e3b5ed5c27a848008abd5beb3d9e6c37c3f33",
+            "http://whatever/job/123",
+            1230.0,
+            10.0,
+        );
+        let report: Report = build_junit_report(&carboncrush_result);
+        println!("{}", report.to_string().unwrap());
+        const EXPECTED_XML: &str = r#"<?xml version="1.0" encoding="UTF-8"?>
+<testsuites name="Power measure" tests="1" failures="0" errors="0">
+    <testsuite name="myapp1" tests="1" disabled="0" errors="0" failures="0">
+        <testcase name="d50e3b5ed5c27a848008abd5beb3d9e6c37c3f33">
+            <system-out>CarbonCrushResult { consumption: &quot;123&quot;, app_id: &quot;myapp1&quot;, duration: &quot;10&quot;, branch: &quot;main&quot;, commit_sha: &quot;d50e3b5ed5c27a848008abd5beb3d9e6c37c3f33&quot;, energy: &quot;1230&quot;, ci_pipeline_url: &quot;http://whatever/job/123&quot; }</system-out>
+        </testcase>
+    </testsuite>
+</testsuites>
+"#;
+        assert_eq!(report.to_string().unwrap(), EXPECTED_XML);
+    }
+    #[test]
+    fn test_save_junit_report() {
+        let carboncrush_result = build_carboncrush_result(
+            123.0,
+            "myapp1",
+            "main",
+            "d50e3b5ed5c27a848008abd5beb3d9e6c37c3f33",
+            "http://whatever/job/123",
+            1230.0,
+            10.0,
+        );
+        let mut junit_report: PathBuf = PathBuf::new();
+        junit_report.push("cc_junit_report.xml");
+        save_as_junit_report(&carboncrush_result, junit_report);
     }
 }
